58c5bdeb9628414646401f92f66e09ca27519d9e
Jared Stevens <jarestev@adobe.com>
2021-09-12 10:45:19 -0600

    Initial commit

---
+++ src/test/java/com/adobe/interviews/servicetest/RomanNumerals/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adobe.interviews.servicetest.RomanNumerals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
